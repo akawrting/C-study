be4a75522d99a454803b67a698848384a3237918
akawrting <simpilgione@gmail.com>
2025-03-12 17:51:12 +0900

    20250312

--- baek.c
+++ baek.c
@@ -1,41 +1,18 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int n, b, c=1;
-    
-    scanf("%d %d", &n,&b);
+int main(void){
 
-    int b1=1;
-    int n1 = n;
-    while(n/b1 >= b){
-        c++;
-        b1*=b;
+    int a, b, v;
+    int day;
+    scanf("%d%d%d", &a, &b, &v);
+
+    if((v%(a-b))==0){
+        day=v/(a-b);
     }
-    int size = c;
-    char arr[size];
-    c-=1;
-    printf("c=%d\n",c);
+    else day=v/(a-b)+1;
 
-    for(int i=c,j=0; i>=0; i--,j++){
-        int d=1;
-
-        for(int k=0; k<i; k++){
-            d*=b;
-        }
-        if(n/d >= 10 && n/d <= 35){
-            arr[j] = n/d +'A'-10;
-        }
-        else arr[j] = n/d;
-        printf("arr%d=%d\n",j,arr[j]);
-        n = n%d;
-    }
-    
-    for(int i=0; i<size; i++){
-        printf("%d",arr[i]);
-    }
-
-    
+    printf("%d",day);
 
     return 0;
+
 }
